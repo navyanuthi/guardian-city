29f8a393a366973d265205b87bb3bebce9868919
navyanuthi <navyanuthi58@gmail.com>
2026-07-15 19:33:57 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/guardian-city/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
